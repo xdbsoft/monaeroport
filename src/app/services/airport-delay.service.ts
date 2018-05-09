c5b3247f8dfc41558af80dce35cfa166b0b1a689
xeonx <simon.hege@gmail.com>
2018-05-09 22:18:34 +0200

    Ajout de ficiers manquants

---
+++ src/app/services/airport-delay.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs/Observable';
+import { AirportDelay } from '../model/airport-delay';
+
+import * as olap from 'olap-cube';
+
+@Injectable()
+export class AirportDelayService {
+
+  constructor(private http: HttpClient) { }
+
+  getDelays(icao: string): Promise<olap.model.Table> {
+
+    if (icao.length != 4) {
+      return new Promise<olap.model.Table>( (resolve, reject) => {
+        reject("Invalid ICAO code");
+      });
+    }
+
+    return this.http.get<any>('assets/'+icao+'_cube_links_delays.json').toPromise().then(v => new olap.model.Table(v));;
+  }
+
+}
