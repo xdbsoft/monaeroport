e44570c21cd4973a9894b72690565471b3376b7e
xeonx <simon.hege@gmail.com>
2018-05-28 20:40:12 +0200

    ChangeDetectionStrategy.OnPush for raw data card

--- src/app/cards/card-raw-data/card-raw-data.component.ts
+++ src/app/cards/card-raw-data/card-raw-data.component.ts
@@ -1,11 +1,14 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ChangeDetectionStrategy, OnChanges, SimpleChanges } from '@angular/core';
+
+import * as olap from 'olap-cube';
 
 @Component({
   selector: 'monapt-card-raw-data',
   templateUrl: './card-raw-data.component.html',
-  styleUrls: ['./card-raw-data.component.css']
+  styleUrls: ['./card-raw-data.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class CardRawDataComponent implements OnInit {
+export class CardRawDataComponent implements OnInit, OnChanges {
 
   @Input()
   title: string;
@@ -14,7 +17,8 @@ export class CardRawDataComponent implements OnInit {
   labels: string[];
 
   @Input()
-  data: any[][];
+  data: olap.model.Table;
+  rows: any[][] = [];
 
   page: number = 1;
   pageSize: number = 10;
@@ -22,6 +26,13 @@ export class CardRawDataComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    console.log("CardRawDataComponent change", changes, this.data);
+    if (this.data) {
+      this.rows = this.data.rows;
+    }
   }
 
 }
